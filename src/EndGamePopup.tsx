1dc47b03e40687c983f8ae5f1f3adb7b48ae9da2
Mark Craven <mpcrav01@gmail.com>
2024-03-08 16:31:43 -0500

    implement countdown timer

--- src/EndGamePopup.tsx
+++ src/EndGamePopup.tsx
@@ -1,5 +1,7 @@
 import { Button, Fade, Modal } from '@mui/material'
 import EmojiResultTable from './components/EmojiResultTable'
+import CountdownTimer from './components/CountdownTimer'
+import { getTimeTilMidnightEastern } from './utils/dateTimeProvider'
 
 export type EndGamePopupProps = {
     guesses: number
@@ -18,7 +20,7 @@ export default function EndGamePopUp(props: EndGamePopupProps) {
         <Modal open={isOpen} onClose={onClose}>
             <Fade in={isOpen}>
                 <div className="center-modal">
-                    <div className="flex flex-col h-96 bg-white items-center p-7 rounded-md gap-7">
+                    <div className="flex flex-col min-h-96 bg-white items-center p-7 rounded-md gap-7">
                         <div className="text-2xl italic">
                             {gameOverAdlibs[0]}
                         </div>
@@ -35,11 +37,19 @@ export default function EndGamePopUp(props: EndGamePopupProps) {
                                 <div>Better luck next time!</div>
                             </div>
                         )}
-                        <div className="flex flex-col text-xl">
-                            <div>Turfle will reset in:</div>
+                        <div className="flex flex-col text-2xl gap-2">
+                            <div className="mx-auto">
+                                <span className="bebas-neue-regular text-3xl mr-1">
+                                    Turfle
+                                </span>
+                                will reset in:
+                            </div>
+                            <CountdownTimer
+                                unixTimeEnds={getTimeTilMidnightEastern()}
+                            ></CountdownTimer>
                         </div>
                         <div className="flex gap-4 justify-between mt-auto mb-2">
-                            <div className="my-auto text-lg">
+                            <div className="my-auto text-xl">
                                 Share Results:
                             </div>
                             <EmojiResultTable></EmojiResultTable>
---
+++ src/components/CountdownTimer.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react'
+
+export type CountdownTimerProps = {
+    unixTimeEnds: number
+}
+
+export type CountdownTimer = {
+    hours: number
+    minutes: number
+    seconds: number
+}
+
+const finishedTimer = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+}
+
+const hourInSeconds = 3600
+export default function CountdownTimer(props: CountdownTimerProps) {
+    const { unixTimeEnds } = props
+    const [remainingTime, setRemainingTime] =
+        useState<CountdownTimer>(finishedTimer)
+
+    useEffect(() => {
+        const timeTil = unixTimeEnds - Date.now() / 1000
+
+        const hours = Math.floor(timeTil / hourInSeconds)
+        const minutes = Math.floor((timeTil % hourInSeconds) / 60)
+        const seconds = Math.floor(
+            timeTil - (hours * hourInSeconds + minutes * 60)
+        )
+
+        const newTimeObj: CountdownTimer = {
+            hours,
+            minutes,
+            seconds,
+        }
+        const timer = setInterval(() => {
+            setRemainingTime(newTimeObj)
+        }, 1000)
+
+        if (timeTil <= 0) {
+            setRemainingTime({ hours: 0, minutes: 0, seconds: 0 })
+            clearInterval(timer)
+        }
+
+        return () => {
+            clearInterval(timer)
+        }
+    })
+
+    return (
+        <div className="flex flex-col">
+            <div className="flex text-5xl bebas-neue-regular mx-auto gap-2">
+                <div>{remainingTime.hours.toString().padStart(2, '0')} :</div>
+                <div>{remainingTime.minutes.toString().padStart(2, '0')} :</div>
+                <div>{remainingTime.seconds.toString().padStart(2, '0')}</div>
+            </div>
+            {remainingTime.hours +
+                remainingTime.minutes +
+                remainingTime.seconds <=
+                0 && (
+                <div className="text-xl mt-2">
+                    Refresh to play today's game!
+                </div>
+            )}
+        </div>
+    )
+}
